874e97cf1d18dafff897c5f77a68e6f93f375d4f
grupobeta <44404558+grupobeta@users.noreply.github.com>
2018-11-13 23:27:41 -0200

    Add files via upload

---
+++ cliente_deleta_produto.py
@@ -0,0 +1,6 @@
+﻿import requests as Req
+
+p = input("Por Favor, informe Qual produto você quer deletar? ")
+url = "http://127.0.0.1:7000/produtos/" + p
+retorno = Req.api.delete(url).json()
+print(retorno)
---
+++ cliente_ver_produto.py
@@ -0,0 +1,6 @@
+﻿import requests as Req
+
+p = input("Por Favor , informe Qual produto você quer ver? ")
+url = "http://127.0.0.1:7000/produtos/" + p
+retorno = Req.api.get(url).json()
+print(retorno)
